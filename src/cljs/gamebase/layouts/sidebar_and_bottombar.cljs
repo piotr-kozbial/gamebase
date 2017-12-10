bde6bef71551bda179664d023cead50cdad3e94e
Piotr Kozbial <piotr.jan.kozbial@gmail.com>
2017-12-10 22:26:37 +0100

    m

--- src/cljs/gamebase/layouts/sidebar_and_bottombar.cljs
+++ src/cljs/gamebase/layouts/sidebar_and_bottombar.cljs
@@ -9,20 +9,27 @@
 
 (declare -setup-events update-canvas-size show-canvas)
 
+;; This is the "constant" part of the state of the layout.
+;; That is, it should be set identically for every game.
+;; It will not be saved with save game.
+(defonce state (atom {}))
+
 ;; API
 
-(defn mk-layout [options]
-  (let [layout (atom {:options options})]
-    (-setup-events layout)
-    layout))
+(defn mk-layout [base-atom kvs opts]
+  (reset! state (assoc opts
+                       :base-atom base-atom
+                       :kvs kvs))
+  (-setup-events)
+  (swap! base-atom update-in kvs (constantly {})))
 
-(defn initialize [layout]
-
-  (let [cnv (js/createCanvas 600 400)]
+(defn initialize []
+  (let [{:keys [base-atom kvs]} @state
+        cnv (js/createCanvas 600 400)]
     (.parent cnv "gamebase/canvas-holder")
-    (update-canvas-size layout)
+    (update-canvas-size)
     (show-canvas)
-    (update-canvas-size layout)))
+    (update-canvas-size)))
 
 (defn get-canvas-holder-element []
   (.getElementById js/document "gamebase/canvas-holder"))
@@ -47,12 +54,13 @@
   (let [canvas-holder (get-canvas-holder-element)]
     (set! (.-display (.-style canvas-holder)) "none")))
 
-(defn get-canvas-rectangle [layout]
-  (if-let [{:keys [canvas-x canvas-y canvas-width canvas-height]}
-           @layout]
-    [[canvas-x canvas-y]
-     [(+ canvas-x canvas-width) (+ canvas-y canvas-height)]]
-    [[0 0] [0 0]]))
+(defn get-canvas-rectangle []
+  (let [{:keys [base-atom kvs]} @state]
+    (if-let [{:keys [canvas-x canvas-y canvas-width canvas-height]}
+             (get-in @base-atom kvs)]
+      [[canvas-x canvas-y]
+       [(+ canvas-x canvas-width) (+ canvas-y canvas-height)]]
+      [[0 0] [0 0]])))
 
 ;; PRIVATE
 
@@ -62,14 +70,13 @@
   (set! (.-left (.-style element)) (str x "px"))
   (set! (.-width (.-style element)) (str width "px")))
 
-(defn update-canvas-size [layout]
-  (let [{:keys [bottom-bar-height
-                after-canvas-resize]} (:options @layout)
+(defn update-canvas-size []
+  (let [{:keys [base-atom kvs bottom-bar-height after-canvas-resize]} @state
         width (.-innerWidth js/window)
         height (.-innerHeight js/window)
         canvas-width (- width 200)
         canvas-height (- height bottom-bar-height)]
-    (.log js/console "update canvas size")
+
     (position-element (get-canvas-holder-element)
                       200 0 canvas-width canvas-height)
     (position-element (get-side-bar-element)
@@ -78,24 +85,16 @@
                       200 (- height bottom-bar-height)
                       (- width 200) bottom-bar-height)
     (js/resizeCanvas canvas-width canvas-height)
-    (swap! layout assoc
+    (swap! base-atom update-in kvs
+           assoc
            :canvas-x 200
            :canvas-y 0
            :canvas-width canvas-width
            :canvas-height canvas-height)
-    (after-canvas-resize)
+    (after-canvas-resize)))
 
-    ;; (swap! global/app-state
-    ;;        (fn [{:keys [top-left-x top-left-y] :as st}]
-    ;;          (let [
-    ;;                [new-top-left-x new-top-left-y]
-    ;;                ,   (correct-top-left top-left-x
-    ;;                                      top-left-y)]
-    ;;            (assoc st :top-left-x new-top-left-x
-    ;;                   :top-left-y new-top-left-y))))
+(defn -setup-events []
+  (let [{:keys [base-atom kvs]} @state]
+    (events/add-handler :window-resized
+                        (fn [_] (update-canvas-size)))))
 
-    ))
-
-(defn -setup-events [layout]
-  (events/add-handler :window-resized ;;setOnWindowResized
-   (fn [_] (update-canvas-size layout))))
